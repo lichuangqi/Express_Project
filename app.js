94af698de5c84100056c16a064ade512d9dc10b5
Charlie <charlie.chuangqi.li@gmail.com>
2022-12-26 14:22:23 +1000

    error handle middleware

--- app.js
+++ app.js
@@ -1,8 +1,19 @@
 const express = require('express')
 const router = require('./router/index')
+const routerVideo = require('./router/video')
 const app = express()
 // 路由前缀
 app.use('/user',router)
+app.use('/video',routerVideo)
+// 当路由都匹配不到，中间件处理404错误
+app.use((req,res,next)=>{
+  res.status(404).send('404 Not Found.')
+})
+// 四个参数的中间件叫错误处理中间件
+app.use((err,req,res,next)=>{
+  console.log(err)
+  res.status(500).send('service error.')
+})
 const PORT = process.env.PORT || 3000
 app.listen(PORT, () => {
   console.log(`Server is running at http://localhost:${PORT}`)
